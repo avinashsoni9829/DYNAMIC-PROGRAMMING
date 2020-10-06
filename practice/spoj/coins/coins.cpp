20468b063a0ad5d859b97800a8820572d574a203
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-10-06 20:17:21 +0530

    Create coins.cpp

---
+++ practice/spoj/coins/coins.cpp
@@ -0,0 +1,76 @@
+// Problem: Bytelandian gold coins
+// Contest: SPOJ - Classical
+// URL: https://www.spoj.com/problems/COINS/
+// Memory Limit: 1536 MB
+// Time Limit: 9000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include<bits/stdc++.h>
+using namespace std;
+const long long int  modu=1e9+7;
+#define endl '\n'
+#define ll long long int
+#define vi vector<int>
+#define vl vector<ll>
+#define inp(x)  for(auto &t:x) cin>>t;
+#define itr     vector<int>::iterator
+#define debug(a) cout << #a << "=" << a << ' '<<endl;
+#define out(x)  for(auto &t:x) cout<<t<<" "; cout<<endl;
+#define ff first
+#define ss second
+#define pb push_back
+#define initialize(a,v)  for(int i=0;i<a.size();++i) a[i]=v;
+#define all(x) x.begin(),x.end()
+#define mpt(mp) for(auto it=mp.begin();it!=mp.end();it++)
+#define trav(x) for(int i=0;i<(int)x.size();++i)
+#define mkp(a,b) make_pair(a,b)
+#define sz(x)  (int)x.size()
+
+int noofdig(int N)
+{
+	return floor(log10(N))+1;
+}
+
+
+ll f(ll a)
+{
+
+return 0;
+
+}
+
+
+
+void pre()
+{
+
+}
+
+ll ans(ll n)
+{
+	if(n==0)
+	return 0;
+	return max(n,ans(n/2)+ans(n/3)+ans(n/4));
+}
+
+
+void solve()
+{
+	ll n;
+	while(cin>>n)
+	cout<<ans(n)<<endl;
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t=1;
+	//cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
