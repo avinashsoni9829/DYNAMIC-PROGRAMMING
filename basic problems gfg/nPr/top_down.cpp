fb4be0011a0f5142cdbb95c689b6f4c915ac510b
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-12-01 04:12:08 -0800

    Create top_down.cpp

---
+++ basic problems gfg/nPr/top_down.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int nPr(int n, int k) 
+{ 
+    int P[n + 1][k + 1]; 
+  
+    // Calculate value of Permutation  
+    // Coefficient in bottom up manner 
+    for (int i = 0; i <= n; i++) 
+    { 
+        for (int j = 0; j <= std::min(i, k); j++) 
+        { 
+            // Base Cases 
+            if (j == 0) 
+                P[i][j] = 1; 
+  
+            // Calculate value using 
+            // previosly stored values 
+            else
+                P[i][j] = P[i - 1][j] +  
+                          (j * P[i - 1][j - 1]); 
+  
+            // This step is important 
+            // as P(i,j)=0 for j>i 
+            P[i][j + 1] = 0; 
+        } 
+    } 
+    return P[n][k]; 
+} 
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll n,r;
+		cin>>n>>r;
+		cout<<nPr(n,r)<<endl;
+	}
+}
